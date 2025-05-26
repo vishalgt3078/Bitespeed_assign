56db0347f416acd36d2efe6e6547ac13f1eb2d24
vishalgt3078 <vishalgupta3078@gmail.com>
2025-05-26 23:53:20 +0530

    refactor: Improve response formatting and update primary logic and added readme file

--- src/routes/contactRoutes.ts
+++ src/routes/contactRoutes.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';
+import express from 'express';
 import { handleIdentifyRequest } from '@/controllers/contactController';
 
-const router = Router();
+const router = express.Router();
 
 router.post('/identify', handleIdentifyRequest);
 
